e0487275e34fcea6489fd16628a68a09001415c3
Ashray-shetty <ashray.shetty@multiverse.io>
2021-11-23 23:01:05 +0000

    removed prev-next footbar

---
+++ _build/jupyter_execute/intro.py
@@ -0,0 +1,38 @@
+# Data Science
+<hr style="height:1px;border:none;color:#666;background-color:#666;" />
+
+```{admonition} Welcome!
+The goal of this website is to teach python programming and machine learning, even without a strong technical background.
+```
+
+With this website I aim to provide an introduction to everything you need to know to start using Python for data science. I am hoping it will act as a codebook, where you can execute the code and make notes on the website. We’ll cover topics such as basic programming, using libraries like Pandas and Matplotlib for data exploration and visualization and topics on Machine Learning.
+
+```{figure} datascience.jpg
+---
+width: 60%
+name: Data Science
+alt: Data Science
+---
+**DIKW Pyramid**
+```
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+Python editor
+```
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: Programming using Python
+
+Chapters/Exercise 01
+Chapters/Solutions 01
+```
